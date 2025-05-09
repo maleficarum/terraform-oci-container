f270390f33d1c11ae222a0ead424c67c83cde07d
Oscar Ivan <o@maleficarum.mx>
2025-05-09 13:03:20 -0600

    feat: Added base structure

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "container_instance" {
+    value = oci_container_instances_container_instance.container_instance
+    description = "The created instance"
+}
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "compartment_id" {
+  type        = string
+  description = "The target compartment"
+}
+
+variable "container_definition" {
+  description = "The container definition"
+  type = object({
+    name  = string,
+    image = string,
+    shape = object({
+      shape  = string,
+      ocpus  = number,
+      memory = number
+    }),
+    resource = object({
+      vcpus_limit         = number,
+      memory_limit_in_gbs = number
+    }),
+    ports = object({
+      port     = number,
+      protocol = string
+    }),
+    volumes = object({
+      mount_path  = string,
+      volume_name = string,
+      size_in_gbs = number
+    }),
+    health_checks = object({
+      type = string,
+      port = number
+    }),
+    environment_variables = map(string)
+    command = list(string)
+  })
+}
+
+variable "subnet" {
+  type        = string
+  description = "The target subnet OCID"
+}
+
+variable "environment" {
+  type        = string
+  description = "The target environment"
+}
